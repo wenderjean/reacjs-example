e4e0e1fa8b4d7b7d8296c468c812a1d74ebda172
Wender Freese <wender.jean@gmail.com>
2016-04-20 19:51:04 -0300

    Adding Header component

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import Header from './Header'
+
 export default class Layout extends React.Component {
   render() {
     return (
-      <h1>It Works!</h1>
+      <div>
+        <Header />
+      </div>
     );
   }
 }
